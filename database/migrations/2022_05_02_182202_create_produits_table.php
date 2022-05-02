a6816d626f01c1e9246d86c20822866db46378e4
Michael <mika.rakotondraibe@gmail.com>
2022-05-02 21:40:22 +0300

    connection avec la base de donnee

--- database/migrations/2022_05_02_182202_create_produits_table.php
+++ database/migrations/2022_05_02_182202_create_produits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProduitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('produits', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('nom');
+            $table->integer('categorie');
+            $table->float('prix');
+            $table->text('description');
+            $table->string('poids');
+        
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produits');
     }
 }
