78b2cb85129786c8641c0126ce42e7d0f0017d6a
Michael <mika.rakotondraibe@gmail.com>
2022-05-07 11:05:20 +0300

    details d'un produit

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/shop',"App\Http\Controllers\ShopController@allProduits")->name('sho
 Route::get('panier',"App\Http\Controllers\PanierController@allProduits")->name('panier');
 Route::get('blog',"App\Http\Controllers\BlogController@allProduits")->name('blog');
 Route::get('/contact',"App\Http\Controllers\ContactController@allProduits")->name('contact');
-Route::get('/produitDetails',"App\Http\Controllers\ProduitDetailsController@details");
+Route::get('/produitDetails/{id}',"App\Http\Controllers\ProduitDetailsController@details")->name('produitDetails');
 Route::get('/blogDetails',"App\Http\Controllers\BlogDetailsController@allProduits");
 Route::get('/categorie/{id}',"App\Http\Controllers\CategorieController@byCategorie")->name('categorie');
